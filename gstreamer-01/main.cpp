420cd1ab61d033c8ed7c458614715c1a1aa612bd
karthick <karthickps.ai@gmail.com>
2020-05-10 18:59:56 +0530

    Code Finished

--- gstreamer-01/main.cpp
+++ gstreamer-01/main.cpp
@@ -18,8 +18,16 @@ int main(int arg, char *argv[]) {
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
     //wait until error or EOS ( End Of Stream )
-    
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                     static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-    std::cout << "Hello, World!" << std::endl;
+    // free memory
+    if (msg != nullptr)
+        gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+
     return 0;
 }
